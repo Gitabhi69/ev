fa8397b2c1f63128eb6d0efbfa4f79e75aa34870
Abhinav <149236922+Gitabhi69@users.noreply.github.com>
2024-08-01 13:05:54 +0545

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("ᴀʙᴏᴜᴛ ᴜꜱ 🥀", callback_data = "about"),
+                    InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data = "close")
                 ]
             ]
         )
